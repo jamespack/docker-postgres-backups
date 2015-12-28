fe5b5d0fd830324d89115496fca5d02c02d34a8f
James Potter <james.potter@gmail.com>
2015-12-28 12:13:58 +0700

    Check for required environment vars.

--- run-backup.sh
+++ run-backup.sh
@@ -1,5 +1,12 @@
-# Credit for some of this goes to: https://github.com/siomiz/PostgreSQL-S3
+# Credit for lots of this goes to: https://github.com/siomiz/PostgreSQL-S3
 set -e # stop if any of these commands fail
+
+: ${POSTGRES_PORT_5432_TCP_ADDR:?"--link to a PostgreSQL container is not set"}
+: ${GPG_PUBKEY_ID:?"-e GPG_PUBKEY_ID is not set"}
+: ${AWS_ACCESS_KEY_ID:?"-e AWS_ACCESS_KEY_ID is not set"}
+: ${AWS_SECRET_ACCESS_KEY:?"-e AWS_SECRET_ACCESS_KEY is not set"}
+: ${S3_BUCKET_NAME:?"-e S3_BUCKET_NAME is not set"}
+
 echo "*** Starting run-backup.sh ***"
 
 
